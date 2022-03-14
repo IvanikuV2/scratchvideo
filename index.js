8edf8d4244827a375ee34504d7b14fb8ce28e9c5
IvanikuV2 <ivaniku@tuta.io>
2022-03-14 19:23:15 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const unzipper = require('unzipper');
+
+const { spawn } = require("child_process");
+
+var prompt = require('prompt-sync')();
+
+const fs = require('fs'); 
+const path = require('path');
+
+const fps = parseInt(prompt("Before getting started, what's your video's framerate? (Will only affect how smooth your video will look) > "))
+
+console.log("Cleaning up...")
+
+// this chunk of code cleans up the /tmp directory for future use :P
+const directory = 'work/tmp/extractedProject';
+
+fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+  
+    for (const file of files) {
+      fs.unlinkSync(path.join(directory, file), err => {
+        if (err) throw err;
+      });
+    }
+});
+
+console.log("Unpacking SB3...")
+
+// unzip sb3 file
+
+fs.createReadStream('work/projects/project.sb3')
+    .pipe(unzipper.Extract({ path: 'work/tmp/extractedProject/' }))
+    .on('close', function () {
+        console.log("\nFinished unpacking!\n")
+        writeandparse()
+    });
+
+async function writeandparse(){
+    console.log("\nParsing main Project file, this may take long depending on your computer specs and project size...\n")
+    let object1 = JSON.parse(fs.readFileSync("work/tmp/extractedProject/project.json"));
+    console.log("Parsing finished!")
+
+    console.log("\nExtracting video frames... (will take a long time!)")
+
+    fs.rmSync("work/tmp/frames", { recursive: true, force: true });
+    fs.mkdirSync("work/tmp/frames/")
+
+    const extractframe = spawn(`ffmpeg -i \"work/projects/video.mp4\" -r ${fps} \"work/tmp/frames/frame-%04d.png\"`)
+
+    extractframe.stdout.on("data", data => {
+        console.log(`stdout: ${data}`);
+    });
+    
+    extractframe.stderr.on("data", data => {
+        console.log(`stderr: ${data}`);
+    });
+
+    extractframe.on('error', (error) => {
+        console.log(`error: ${error.message}`);
+    });
+    
+    extractframe.on("close", code => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+    console.log("\nVideo frames extracted, parsing images...")
+}
