90b543567a52e525967ea115bb75f09db74aae76
IvanikuV2 <ivaniku@tuta.io>
2022-03-16 13:26:24 +0100

    Revamped prompt

--- index.js
+++ index.js
@@ -2,40 +2,53 @@ const unzipper = require('unzipper');
 
 const { spawn } = require("child_process");
 
-var prompt = require('prompt-sync')();
+var prompt = require('prompts');
 
 const fs = require('fs'); 
 const path = require('path');
 
-const fps = parseInt(prompt("Before getting started, what's your video's framerate? (Will only affect how smooth your video will look) > "))
+var fps
 
-console.log("Cleaning up...")
-
-// this chunk of code cleans up the /tmp directory for future use :P
-const directory = 'work/tmp/extractedProject';
-
-fs.readdir(directory, (err, files) => {
-    if (err) throw err;
+(async () => {
+    const response = await prompt({
+      type: 'number',
+      name: 'value',
+      message: 'What will be the video\'s FPS',
+    });
   
-    for (const file of files) {
-      fs.unlinkSync(path.join(directory, file), err => {
+    fps = response.value
+    unpack()
+})();
+
+function unpack(){
+    console.log("Cleaning up...")
+
+    // this chunk of code cleans up the /tmp directory for future use :P
+    const directory = 'work/tmp/extractedProject';
+
+    fs.readdir(directory, (err, files) => {
         if (err) throw err;
-      });
-    }
-});
-
-console.log("Unpacking SB3...")
-
-// unzip sb3 file
-
-fs.createReadStream('work/projects/project.sb3')
-    .pipe(unzipper.Extract({ path: 'work/tmp/extractedProject/' }))
-    .on('close', function () {
-        console.log("\nFinished unpacking!\n")
-        extractframes()
+    
+        for (const file of files) {
+        fs.unlinkSync(path.join(directory, file), err => {
+            if (err) throw err;
+        });
+        }
     });
 
-async function extractframes(){
+    console.log("Unpacking SB3...")
+
+    // unzip sb3 file
+
+    fs.createReadStream('work/projects/project.sb3')
+        .pipe(unzipper.Extract({ path: 'work/tmp/extractedProject/' }))
+        .on('close', function () {
+            console.log("\nFinished unpacking!\n")
+            extractframes()
+        });
+}
+
+function extractframes(){
     console.log("\nParsing main Project file, this may take long depending on your computer specs and project size...\n")
     let object1 = JSON.parse(fs.readFileSync("work/tmp/extractedProject/project.json"));
     console.log("Parsing finished!")
@@ -53,6 +66,23 @@ async function extractframes(){
     
     extractframe.on("close", code => {
         console.log(`child process exited with code ${code}`);
-        console.log("\nVideo frames extracted, parsing images...")
+        console.log("\nVideo frames successfully extracted!")
+        parseimages()
+    });
+}
+
+function parseimages(){
+    console.log("Parsing images for json prep")
+    const directoryPath = path.join(__dirname, 'work/tmp/frames');
+    //passsing directoryPath and callback function
+    fs.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to list image files: ' + err);
+        } 
+        //listing all files using forEach
+        let images = (fs.readdirSync(directoryPath))
+
+        console.log(files)  
     });
 }
